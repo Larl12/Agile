893b63df3a71fada3f6a89c68dafc3d2507f78a7
Semen <semen.k.33@mail.ru>
2026-04-22 13:12:44 +0300

    Update Player.cs

--- StarfallArena/Player.cs
+++ StarfallArena/Player.cs
@@ -1,5 +1,14 @@
 namespace StarfallArena;
 
 public class Player
-{
+{    
+    public string Name { get; set; } = "Hero";
+
+    public int Health { get; set; } = 10;
+
+    public int Armor { get; set; } = 0;
+
+    public string CharacterClass { get; set; } = "Adventurer";
+
+    public string StartingWeapon { get; set; } = "Training Sword";
 }
