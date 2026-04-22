78e7be3bfdf018356d7e9426154759dcf814c320
Semen <semen.k.33@mail.ru>
2026-04-22 13:41:04 +0300

    Fix player builder integration in dev

--- StarfallArena/Entities/Player.cs
+++ StarfallArena/Entities/Player.cs
@@ -5,9 +5,24 @@ public class Player : Entity
     public Player(string name, int health, int x, int y)
         : base(name, health, x, y, '@')
     {
+        CharacterClass = "Adventurer";
+        StartingWeapon = "Training Sword";
     }
 
+    public int Armor { get; private set; }
+
+    public string CharacterClass { get; private set; }
+
+    public string StartingWeapon { get; private set; }
+
     public int Score { get; private set; }
+
+    public void ConfigureLoadout(int armor, string characterClass, string startingWeapon)
+    {
+        Armor = armor;
+        CharacterClass = characterClass;
+        StartingWeapon = startingWeapon;
+    }
 
     public void AddScore(int points)
     {
